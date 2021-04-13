d971b676d285946fd3b412127e7e1685b1f2c879
Tavrin <etienne.doux@gmail.com>
2021-04-13 05:14:35 +0200

    DEV-17: post crud, form cleaning, pagination

--- src/Forms/ResetPasswordForm.php
+++ src/Forms/ResetPasswordForm.php
@@ -13,9 +13,9 @@ class ResetPasswordForm extends \Core\controller\Form
     {
         parent::__construct($request, $entity, $session, $options);
 
-        $form->addPasswordInput('password', ['required' => true, 'class' => 'form-control', 'placeholder' => 'Nouveau mot de passe', 'fieldName' => 'password', 'hash' => true]);
-        $form->addPasswordInput('passwordConfirm', ['entity' => false, 'required' => true, 'class' => 'form-control', 'label' => 'Confirmation','placeholder' => 'Confirmation de nouveau mot de passe']);
-        $form->setSubmitValue('Changer le mot de passe', ['class' => 'button-bb-wc']);
+        $this->addPasswordInput('password', ['required' => true, 'class' => 'form-control', 'placeholder' => 'Nouveau mot de passe', 'fieldName' => 'password', 'hash' => true]);
+        $this->addPasswordInput('passwordConfirm', ['entity' => false, 'required' => true, 'class' => 'form-control', 'label' => 'Confirmation','placeholder' => 'Confirmation de nouveau mot de passe']);
+        $this->setSubmitValue('Changer le mot de passe', ['class' => 'button-bb-wc']);
 
     }
 }
